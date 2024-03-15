c64fc38736b65a7033b87048c02c2147dee25f3e
2361768642 <155428413+2361768642@users.noreply.github.com>
2024-03-15 22:36:19 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function toggleVisibility() {
+  var content = document.getElementById("content");
+  if (content.style.display === "none") 
+  {
+    content.style.display = "block";
+  } 
+  else 
+  {
+    content.style.display = "none";
+  }
+}
