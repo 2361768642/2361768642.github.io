5c1a9980a7c9cbaf685f56a49dc087f05d75390d
2361768642 <155428413+2361768642@users.noreply.github.com>
2025-08-04 17:37:21 +0800

    Update WebData.js

--- WebData.js
+++ WebData.js
@@ -6,7 +6,65 @@ var vm = new Vue({
             window.open(webUrl, '_blank')
         }
     },
+    data: {// <-- Vue.js -->
+var vm = new Vue({
+    el: '#WebStation-box',
+    methods: {
+        jumpWeb: function(webUrl) {
+            window.open(webUrl, '_blank')
+        }
+    },
     data: {
+        webData: [
+            { // 第1行
+                webName: 'QQ邮箱',
+                webUrl: 'https://mail.qq.com',
+            },{
+                webName: 'BiliBili',
+                webUrl: 'https://www.bilibili.com/',
+            },{
+                webName: '秘塔AI搜索',
+                webUrl: 'https://metaso.cn/?s=bdpc',
+            },{
+                webName: '蜜柑计划',
+                webUrl: 'https://mikanani.me/',
+            },{
+                webName: '李府',
+                webUrl: 'https://mym04.github.io/',
+            },{ // 第2行
+                webName: 'HackWay技术路线',
+                webUrl: 'https://hackway.org/',
+            },{
+                webName: 'ChatGPT',
+                webUrl: 'https://chatgpt.com/',
+            },{
+                webName: 'GitHub',
+                webUrl: 'https://github.com/',
+            },{
+                webName: 'Deepseek',
+                webUrl: 'https://chat.deepseek.com/sign_in',
+            },{
+                webName: 'Excalidraw画布',
+                webUrl: 'https://excalidraw.com/',
+            },{ // 第3行
+                webName: 'webpack',
+                webUrl: 'https://webpack.docschina.org/',
+            },{ 
+                webName: 'Electron',
+                webUrl: 'https://www.electronjs.org/zh/',
+            },{
+                webName: 'Vue',
+                webUrl: 'https://cn.vuejs.org/',
+            },{
+                webName: 'Vite',
+                webUrl: 'https://vite.dev/',
+            },{
+                webName: 'ElementPlus',
+                webUrl: 'https://element-plus.org/zh-CN/',
+            },
+        ]
+    },
+});
         webData: [
             { // 第1行
                 webName: 'BiliBili',
@@ -56,4 +114,5 @@ var vm = new Vue({
             },
         ]
     },
+
 });
